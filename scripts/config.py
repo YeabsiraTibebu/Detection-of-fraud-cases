0e5b198ca859f0f164377607a6e1479af2edfd9d
Yeabsira Tibebu <61262880+YeabsiraTibebu@users.noreply.github.com>
2024-06-19 19:37:32 +0300

    create

---
+++ scripts/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+class Config:
+  RANDOM_SEED = 90
+  ASSETS_PATH = Path("../")
+  REPO = "~/Documents/10 academy/Week 8/Detection-of-fraud-cases"
+  DATASET_FILE_PATH = "data/data.csv"
+  DATASET_PATH = ASSETS_PATH / "data"
+  FEATURES_PATH = ASSETS_PATH / "features"
+  MODELS_PATH = ASSETS_PATH / "models"
